a9d68781a35a4398c3323a675c8f78e4fdceb8e1
Jack Stanek <stanekjack@gmail.com>
2016-04-09 15:01:46 -0500

    Add stub test

---
+++ test/test_checker.py
@@ -0,0 +1,10 @@
+import pytest
+
+# TODO: Implement real tests!
+#
+# Right now this is just here as a stub so that we at least have some
+# test for Travis to go through. We want complete test coverage,
+# eventually.
+
+def test_checker():
+    assert True
