423d3cf4ae31a64dc99c714d8d61f8fc894df4e1
Jack Stanek <stanekjack@gmail.com>
2016-04-13 13:20:26 -0500

    Change author info

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='BotBot',
       version='0.0.1',
       description='Laboratory computational resource management',
-      author='Rob Schaefer',
-      author_email='schae234@umn.edu',
+      author='Jack Stanek',
+      author_email='stane064@umn.edu',
       url='http://csbio.cs.umn.edu/schae234',
       packages=[],
      )
