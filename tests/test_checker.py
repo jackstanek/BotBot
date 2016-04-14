3f87a8679a39f8422b013d157d1e93bdfd47d315
Jack Stanek <stanekjack@gmail.com>
2016-04-14 16:57:07 -0500

    Add basic test for fastq file checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,10 +1,16 @@
 import pytest
 
+from botbot import checker, problems
+
 # TODO: Implement real tests!
 #
 # Right now this is just here as a stub so that we at least have some
 # test for Travis to go through. We want complete test coverage,
 # eventually.
 
-def test_checker():
-    assert True
+def test_fastq_checker():
+    bad = checker.is_fastq("bad.fastq")
+    good = checker.is_fastq("good.py")
+
+    assert bad == problems.PROB_FILE_IS_FASTQ
+    assert good == problems.PROB_NO_PROBLEM
