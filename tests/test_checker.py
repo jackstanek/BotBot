230bb0a09146cd0b696b528b3ad6dd9ccf057113
Jack Stanek <stanekjack@gmail.com>
2016-04-23 11:28:22 -0500

    Add test for symlink detection

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -3,10 +3,22 @@ import os, stat
 
 from botbot import checker, problems
 
-def test_fastq_checker():
+def test_fastq_checker_path_names():
     assert checker.is_fastq("bad.fastq") == problems.PROB_FILE_IS_FASTQ
     assert checker.is_fastq("good.py") == problems.PROB_NO_PROBLEM
     assert checker.is_fastq("fastq.actually_ok_too") == problems.PROB_NO_PROBLEM
+
+def test_fastq_checker_symlinks(tmpdir):
+    prev = tmpdir.chdir()
+
+    # Make a test file
+    p = tmpdir.join("bad.fastq")
+    p.write('')
+    os.symlink(p.basename, "good.fastq")
+
+    assert checker.is_fastq("bad.fastq") == problems.PROB_FILE_IS_FASTQ
+    assert checker.is_fastq("good.fastq") == problems.PROB_NO_PROBLEM
+    prev.chdir()
 
 def test_permission_checker(tmpdir):
     # Create a test file
