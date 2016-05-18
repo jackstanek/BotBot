51f73ae328e34e31878467a18bf82c2411c572db
Jack Stanek <stane064@umn.edu>
2016-05-18 16:47:22 -0500

    Fix periods

--- botbot/problems.py
+++ botbot/problems.py
@@ -8,10 +8,10 @@ class Problem:
         self.fix = fix
 
 every_problem = {
-    'PROB_DIR_NOT_WRITABLE': Problem(1, 'Directory not writable.', 'Run \'chmod 0655\' on this directory.'),
-    'PROB_FILE_NOT_GRPRD': Problem(2, 'File not group readable.', 'Run \'chmod 0644\' on this file.'),
-    'PROB_FILE_IS_FASTQ': Problem(3, 'File is a fastq file.', 'Don\'t copy fastq files, instead use \'ln -s\''),
-    'PROB_BROKEN_LINK': Problem(4, 'Symbolic link points to nonexistant file.', 'Delete this link.'),
-    'PROB_SAM_SHOULD_COMPRESS': Problem(5, '*.sam files are large.', 'Consider compressing.'),
-    'PROB_SAM_AND_BAM_EXIST': Problem(6, 'The *.sam file has been compressed, but it still exists.', 'Delete the uncompressed copy.'),
+    'PROB_DIR_NOT_WRITABLE': Problem(1, 'Directory not writable', 'Run \'chmod 0655\' on this directory'),
+    'PROB_FILE_NOT_GRPRD': Problem(2, 'File not group readable.', 'Run \'chmod 0644\' on this file'),
+    'PROB_FILE_IS_FASTQ': Problem(3, 'File is a fastq file', 'Don\'t copy fastq files, instead use \'ln -s\''),
+    'PROB_BROKEN_LINK': Problem(4, 'Symbolic link points to nonexistant file', 'Delete this link'),
+    'PROB_SAM_SHOULD_COMPRESS': Problem(5, '*.sam files are large', 'Consider compressing'),
+    'PROB_SAM_AND_BAM_EXIST': Problem(6, 'The *.sam file has been compressed, but it still exists', 'Delete the uncompressed copy'),
 }
