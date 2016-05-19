f05425e4318ef4738571a3f40df20d8a1ab61954
Jack Stanek <stanekjack@gmail.com>
2016-05-19 16:02:57 -0500

    Finish writing tests for the other new checkers

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -12,7 +12,7 @@ def test_group_readable_checker(tmpdir):
     prev = tmpdir.chdir()
 
     # Change its permissions a bunch... maybe this is too expensive?
-    for m in range(0o300, 0o700, 0o010):
+    for m in range(0o000, 0o700, 0o001):
         p.chmod(m)
         prob = schecks.file_groupreadable(p.basename)
         if not bool(0o040 & m): # octal Unix permission for 'group readable'
@@ -23,7 +23,28 @@ def test_group_readable_checker(tmpdir):
     prev.chdir()
 
 def test_dir_exececutable_checker(tmpdir):
-    pass
+    prev = tmpdir.chdir()
+    p = tmpdir.mkdir('bad_dir')
+
+    for m in range(0o000, 0o700, 0o001):
+        p.chmod(m)
+        prob = schecks.dir_group_readable(p.basename)
+        if not bool(0o010 & m): # octal Unix permission for 'group executable'
+            assert prob == 'PROB_DIR_NOT_ACCESSIBLE'
+        elif not bool(0o020 & m):
+            assert prob == 'PROB_DIR_NOT_WRITABLE'
+        else:
+            assert prob is None
 
 def test_file_grp_exec_checker(tmpdir):
-    pass
+    p = tmpdir.join('bad_exec')
+    p.write('')
+    prev = tmpdir.chdir()
+
+    for m in range(0o000, 0o700, 0o001):
+        p.chmod(m)
+        prob = schecks.file_group_executable(p.basename)
+        if bool(0o100 & m) and not bool(0o010 & m):
+            assert prob == 'PROB_FILE_NOT_GRPEXEC'
+        else:
+            assert prob is None
