b187ba9cc68367d970a4a9a7485bdab7c2c47401
Jack Stanek <stanekjack@gmail.com>
2016-05-19 15:14:13 -0500

    Fix up botbot.py:main()

--- botbot/botbot.py
+++ botbot/botbot.py
@@ -6,16 +6,20 @@ def main():
 
     # Verbosity options
     verbosity = parser.add_mutually_exclusive_group()
-    verbosity.add_argument("-v", "--verbose",
-                           help="Print issues and fixes for all files",
-                           action="store_true")
+    verbosity.add_argument('-v', '--verbose',
+                           help='Print issues and fixes for all files',
+                           action='store_true')
 
     # Directory options
-    parser.add_argument("-s", "--shared",
-                        help="Use the shared folder ruleset",
-                        action="store_true")
+    parser.add_argument('path',
+                        help='Path to check')
+    parser.add_argument('-s', '--shared',
+                        help='Use the shared folder ruleset',
+                        action='store_true')
 
-
+    parser.add_argument('-l', '--follow-symlinks',
+                        help='Follow symlinks',
+                        action='store_true')
     # Initialize the checker
     args = parser.parse_args()
 
@@ -27,12 +31,10 @@ def main():
         clist += [schecks.file_groupreadable,
                   schecks.file_group_executable,
                   schecks.dir_group_readable]
+    c.register(clist)
 
     # Check the given directory
-    c.check_tree(args.path, args.link)
+    c.check_tree(args.path, args.follow_symlinks)
 
     # Print the list of issues
     c.pretty_print_issues(args.verbose)
-
-if __name__ == '__main__':
-    main()
