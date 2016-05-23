858d9902874a9b47be80c96b71c0b8bdddc77c55
Jack Stanek <stane064@umn.edu>
2016-05-23 15:02:24 -0500

    Generalize output selection

--- botbot/report.py
+++ botbot/report.py
@@ -14,14 +14,25 @@ class ReportWriter():
         write to the file whose path is specified in out.
         """
         self.chkr = chkr
-        self.out = out
+        self.out = out # Can be a path or sys.stdout
 
-    def write_generic_report(self):
+    def write_dst(self, fn):
+        """
+        Write the output of the report generator to the correct
+        destination object. eg, if we want to write to stdout, no need
+        to open a file. But open the file that we want if there is a
+        file.
+
+        """
+        if self.out == sys.stdout:
+            fn(self.out)
+        else:
+            with open(self.out) as outfile:
+                fn(outfile)
+
+    @write_dst
+    def write_generic_report(self, outfile):
         """Generate a standard report and write to the stream"""
-        outfile = sys.stdout
-        if self.out is not None:
-            outfile = open(self.out, mode='w')
-
         for prob in iter(problems.every_problem):
             if len(self.chkr.probs.files_with_problem(prob)) > 0:
                 print('\n{}'.format(problems.every_problem[prob].message), file=outfile)
@@ -32,6 +43,6 @@ class ReportWriter():
                         start, end = '\t\033[1;37m', '\033[0m'
                         print('{}{}{}, owned by you'.format(start, fileprobs.fi.path, end))
                     else:
-                        print('{}'.format(os.path.abspath(fileprobs.fi.path)))
+                        print('{}, owned by {}'.format(os.path.abspath(fileprobs.fi.path), owner))
 
         outfile.close()
