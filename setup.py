4a2d68b13647a5ed2ecdc60a0dd3f4ec413327f7
Jack Stanek <stane064@umn.edu>
2016-06-08 14:51:23 -0500

    Fix botbot crashing after install w/out config etc.

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@
 
 from setuptools import setup
 
-setup(name='BotBot',
-      version='0.0.4',
-      description='Laboratory computational resource management',
-      author='Jack Stanek',
-      author_email='stane064@umn.edu',
-      url='http://github.com/jackstanek/BotBot',
-      entry_points={
-          'console_scripts': [
-              'botbot = botbot.botbot:main',
-          ]
-      },
-      packages=['botbot'],
-      include_package_data=True)
+setup(
+    name='BotBot',
+    version='0.0.4',
+    description='Laboratory computational resource management',
+    author='Jack Stanek',
+    author_email='stane064@umn.edu',
+    url='http://github.com/jackstanek/BotBot',
+    entry_points={
+        'console_scripts':[
+            'botbot = botbot.botbot:main',
+        ]
+    },
+    packages=['botbot'],
+    include_package_data=True,
+    install_requires=[
+        'jinja2',
+        'pytest'
+    ]
+)
