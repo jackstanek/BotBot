e2d9cf7f9ff58f9912f372dc86da2f5569ced7b1
Jack Stanek <stanekjack@gmail.com>
2016-06-16 16:13:10 -0500

    Fix Travis install

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
 
 setup(
     name='BotBot',
-    version=find_version(),
+    version=find_version('botbot', '__init__.py'),
     description='Laboratory computational resource management',
     author='Jack Stanek',
     author_email='stane064@umn.edu',
