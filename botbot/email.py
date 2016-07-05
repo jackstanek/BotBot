8d24bc2469bd28a51ae6c32fefe942f6acc13fab
Jack Stanek <stane064@umn.edu>
2016-07-05 11:22:14 -0500

    Begin emailing infrastructure

--- botbot/email.py
+++ botbot/email.py
@@ -7,27 +7,51 @@ from botbot.report import ReporterBase
 from botbot.config import CONFIG
 from botbot.problems import every_problem
 
+from jinja2 import Environment
+
 class EmailReporter(ReporterBase):
     """Sends emails to people if they have bad files"""
-    def __init__(self, chkr):
+    def __init__(self, chkr, smtpprovider=smtplib.SMTP):
         super().__init__(chkr)
         self.email = CONFIG.get('email', 'email') # TODO: Add this to the config
         self.passwd = CONFIG.get('email', 'password')
+        self.smtpprovider = smtpprovider # Object that sends emails (can be test dummy)
 
     def _get_user_email_address(self, username):
+        """Construct an email address from an MSI username"""
         domain = CONFIG.get('email', 'domain')
         email = '{}@{}'.format(username, domain)
         return email
 
-    def _prettify_problems(self, fi):
-        return tuple()
+    def _prettify_problems(self, fis):
+        """
+        Get a list of tuples of the problem description, solution, and
+        list of paths
+        """
+        # Get a list of problems that are present in the file list
+        pp = set.union(*[p['problems'] for p in fis]) # present problems
+
+        # Alias for the problem description dictionary
+        ep = every_problem
+
+        # Ugly list comprehension coming up
+        return [(ep[prob].message, ep[prob].fix,
+                 [p['path'] for p in fis if prob in p['problems']])
+                for prob in pp]
+        # Wonderful, innit
 
     def write_report(self, fmt, shared, attr='owner'):
-        send = SMTP(CONFIG.get('email', 'smtp_server'),
-                    CONFIG.get('email', 'smtp_port'))
+        """Send an email to every"""
+        send = self.smtpprovider(CONFIG.get('email', 'smtp_server'),
+                                 CONFIG.get('email', 'smtp_port'))
 
         send.starttls()
         send.login(self.email, self.passwd)
+        allfiles = self.chkr.db.get_files_by_attribute(attr)
 
-        for user, filelist in self.chkr.db.get_files_by_attribute(attr):
-            pass
+        for user in allfiles:
+            filelist = allfiles[user]
+            prettylist = self._prettify_problems(filelist)
+
+            env = self._get_template()
+            msgcontent = env.
--- botbot/report.py
+++ botbot/report.py
@@ -25,7 +25,7 @@ class ReporterBase():
             print('\n', end='')
         sys.stdout.flush()
 
-    def get_template_filename(self, name):
+    def _get_template_filename(self, name):
         """Find the filename of a template. Can be a filename or just a name."""
         parts = str(name).split('.')
         if parts[len(parts) - 1] == 'txt':
@@ -37,6 +37,15 @@ class ReporterBase():
         """Write a report. This base is just a stub."""
         pass
 
+    def _get_template(self, tmp_respath):
+        if resource_exists(__package__, tmp_respath):
+            return Environment(
+                loader=FileSystemLoader(resource_filename(__package__, tmp_respath)),
+                trim_blocks=True
+            )
+        else:
+            raise FileNotFoundError('No such template')
+
 class OneshotReporter(ReporterBase):
     """Does one-off reports after one-off checks"""
     def __init__(self, chkr, out=sys.stdout):
@@ -46,47 +55,40 @@ class OneshotReporter(ReporterBase):
     def write_report(self, fmt, shared, attr='problems'):
         """Write the summary of what transpired."""
         # Find the template
-        tmpname = self.get_template_filename(fmt)
+        tmpname = self._get_template_filename(fmt)
         tmp_respath = os.path.join('resources', 'templates')
 
-        if resource_exists(__package__, tmp_respath):
-            filelist = self.chkr.db.get_files_by_attribute(self.chkr.path, attr, shared=shared)
+        filelist = self.chkr.db.get_files_by_attribute(self.chkr.path, attr, shared=shared)
 
-            # Prune unwanted listings
-            filelist = prune_empty_listings(filelist, attr)
-            if not shared:
-                filelist = prune_shared_probs(filelist, attr)
+        # Prune unwanted listings
+        filelist = prune_empty_listings(filelist, attr)
+        if not shared:
+            filelist = prune_shared_probs(filelist, attr)
 
-            if should_print_report(filelist):
-                env = Environment(
-                    loader=FileSystemLoader(resource_filename(__package__, tmp_respath)),
-                    trim_blocks=True
-                )
+        if should_print_report(filelist):
+            env = self._get_template(tmp_respath)
 
-                tempgen = env.get_template(tmpname).generate({
-                    'attr': attr,
-                    'values': filelist,
-                    'status': self.chkr.status
-                })
+            tempgen = env.get_template(tmpname).generate({
+                'attr': attr,
+                'values': filelist,
+                'status': self.chkr.status
+            })
 
-                if self.out != sys.stdout:
-                    print('Writing report to {}.'.format(self.out))
-                    out = open(self.out, mode='w')
-                else:
-                    print('Report:')
-                    out = sys.stdout
-
-                for line in tempgen:
-                    print(line, file=out, end='')
-
-                print('\n', file=out, end='')
-                out.close()
-
+            if self.out != sys.stdout:
+                print('Writing report to {}.'.format(self.out))
+                out = open(self.out, mode='w')
             else:
-                print('No problems here!')
+                print('Report:')
+                out = sys.stdout
+
+            for line in tempgen:
+                print(line, file=out, end='')
+
+            print('\n', file=out, end='')
+            out.close()
 
         else:
-            raise FileNotFoundError('No such report format')
+            print('No problems here!')
 
 def prune_shared_probs(fl, attr):
     """Remove shared problem listings"""
