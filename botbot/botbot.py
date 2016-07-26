b402b7927c9d51d4a27db565bdbf51f8d4a5fc05
Jack Stanek <stanekjack@gmail.com>
2016-07-26 15:49:43 -0500

    Factor out parser initialization

--- botbot/botbot.py
+++ botbot/botbot.py
@@ -1,18 +1,19 @@
 """Main method"""
-import argparse
 import sys
 
 from botbot import __version__
 from . import checks, schecks, checker, sqlcache
 from . import ignore as ig
 from . import daemon
-from . import config
 
 def config_sanity_check():
     pass # TODO: implement
 
-def main():
-    parser = argparse.ArgumentParser()
+def initialize_parser():
+    """Create a big 'ol argument parser"""
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
 
     sp = parser.add_subparsers(dest='cmd')
     sp.required = True
@@ -49,6 +50,22 @@ def main():
     ## Output options
     fs.add_argument('-o', '--out',
                     help='Write report to a file instead of stdout')
+
+    return parser
+
+
+def main():
+    # Right off the bat, we want to do a quick sanity check on the
+    # configuration file. Basically, make sure we have all the
+    # required fields, and that the
+    try:
+        config.config_sanity_check()
+    except:
+        #TODO: Implement
+        pass
+
+    # Create the argument parser
+    parser = initialize_parser()
 
     # Initialize the checker
     args = parser.parse_args()
